3db48975bee260f7e2d7f69c85d98fa0a0cbe9e1
Miguel <miguelfuenzalida.n@gmail.com>
2020-12-07 16:33:02 -0300

    Rest Server - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/CafeDB';
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URI;
 }
